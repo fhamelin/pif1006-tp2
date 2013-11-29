7189af0edcf674d3e621c89d8328129ee0ed160c
fhamelin <fredhamelin91@gmail.com>
2013-11-29 09:19:13 -0500

    Structure de base du devoir

---
+++ Chiffrement.java
@@ -0,0 +1,13 @@
+
+public class Chiffrement 
+{
+	public static String chiffrer(String message, String cle)
+	{
+		return "";
+	}
+	
+	public static String dechiffrer(String message, String cle)
+	{
+		return "";
+	}
+}
