594bee7a1693afc2ed33c5439192bbe5b2fc546a
Simoniz <simonblanchette4@gmail.com>
2013-12-03 15:12:49 -0500

    Inplémentation de la décription pour CBD, reste a resoudre le bug dans la décription par transposer

--- Chiffrement.java
+++ Chiffrement.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 public abstract class Chiffrement 
 {
+	private static byte VI = 111;
+	
 	public static String chiffrer(String message, String cle)
 	{
 		String msgTransposee;
@@ -17,11 +19,21 @@ public abstract class Chiffrement
 	
 	public static String dechiffrer(String message, String cle)
 	{
-		return "";
+		String msgTransposee;
+		String msgInitial;
+		
+		msgTransposee = DechiffrementCBC(message);
+		msgInitial    = DechiffrementTranspose(msgTransposee, cle);
+		
+		return msgInitial;
 	}
 	
+	// =======================================================================================
+	// Méthodes privés pour le chiffrement
+	// =======================================================================================
+	
 	// Transforme la string dans un tableau 2 dimentions de char
-	private static char[][] GenererTabChar(String _message, int _longCle) {
+	private static char[][] GenererCrypteTabChar(String _message, int _longCle) {
 		
 		int longMsgs, nbLnChar, posMsg;
 		longMsgs = _message.length();
@@ -43,7 +55,7 @@ public abstract class Chiffrement
 	}
 	
 	// gÃ©nÃ¨re un mapping: clef => code de chiffrement; valeur => position dans la Matrice
-	private static Map GenererTabAsso(String _cles, int _longCle) {
+	private static Map GenererMapCrypte(String _cles, int _longCle) {
 		Map<Integer, Integer> posClefs = new HashMap<Integer,Integer>();
 		
 		for (int i = 0; i < _longCle; i++)
@@ -61,8 +73,8 @@ public abstract class Chiffrement
 		sCles    = _cle.replaceAll("\\s","");
 		longCles = sCles.length();
 		
-		Map<Integer,Integer> posClefs = GenererTabAsso(sCles, longCles);
-		messOrigine = GenererTabChar(_message, longCles);
+		Map<Integer,Integer> posClefs = GenererMapCrypte(sCles, longCles);
+		messOrigine = GenererCrypteTabChar(_message, longCles);
 		
 		for (int colonne = 0; colonne < messOrigine[0].length; colonne++) {
 			// Se positionne sur la bonne colonne de la matrice 
@@ -78,19 +90,97 @@ public abstract class Chiffrement
 
 	// Chiffrement par bloc CBC
 	private static String ChiffrementCBC(String _msgTranspose) {
-		byte VI = 111;
 		
-		byte[] bMsgTrans = _msgTranspose.getBytes();
+		byte[] bMsgTrans = _msgTranspose.getBytes(); 
 		byte[] bMsgCBC   = new byte[bMsgTrans.length];
 		
-		for (int i = 0; i < bMsgTrans.length; i++){
+		for (int i = 0; i < bMsgTrans.length; i++){ // bMsgTrans.length
 			if (i == 0) 
 				bMsgCBC[i] = (byte)(((int)bMsgTrans[i]) ^ ((int)VI));
 			else
-				bMsgCBC[i] = (byte)(((int)bMsgTrans[i]) ^ ((int)bMsgTrans[i-1]));
+				bMsgCBC[i] = (byte)(((int)bMsgTrans[i]) ^ ((int)bMsgCBC[i-1]));
 		}
 		
 		return new String(bMsgCBC);
 	}
 	
+	// =======================================================================================
+	// Méthodes privés pour le décriffrement
+	// =======================================================================================
+	
+	// Déchiffrement par CBC
+	private static String DechiffrementCBC(String _message) {
+		
+		byte[] bMsgCBC   = _message.getBytes();
+		byte[] bMsgTrans = new byte[bMsgCBC.length];
+		
+		for (int i = 0; i < bMsgCBC.length; i++){
+			if (i == 0)
+				bMsgTrans[i] = (byte)(((int)bMsgCBC[i]) ^ ((int)VI));
+			else
+				bMsgTrans[i] = (byte)(((int)bMsgCBC[i]) ^ ((int)bMsgCBC[i-1]));
+		}
+		
+		return new String(bMsgTrans);
+	}
+	
+	// Déchiffrement par transposée
+	private static String DechiffrementTranspose(String _msgTrans, String _cle) {
+		
+		String sCles, msgDecodee = "";
+		int longCles, colMatrice;
+		char[][] msgTrans;
+		char[][] msgInitial;
+		
+		sCles    = _cle.replaceAll("\\s","");
+		longCles = sCles.length();
+		
+		Map<Integer,Integer> posClefs = GenererMapDecrypte(sCles, longCles);
+		msgTrans = GenererDecrypteTabChar(_msgTrans, longCles);
+		msgInitial = new char[msgTrans.length][msgTrans[0].length];
+
+		/*
+		for (int colonne = 0; colonne < msgTrans.length; colonne++) {
+			// Se positionne sur la bonne colonne de la matrice 
+			colMatrice = posClefs.get(colonne);
+		
+			for (int ligne = 0; ligne < msgTrans[0].length; ligne++) 
+				if (msgTrans[ligne][colMatrice] != '"')
+					msgInitial[ligne][colMatrice-1] += msgTrans[ligne][colMatrice];		
+		}
+		*/
+		return msgDecodee;
+	}
+	
+	private static char[][] GenererDecrypteTabChar(String _msgTrans, int _longCle) {
+		
+		int longMsgs, nbLnChar, posMsg;
+		longMsgs = _msgTrans.length();
+		nbLnChar = (longMsgs / _longCle) + 1;
+		posMsg   = 0;
+		
+		char[][] cMsgs = new char[nbLnChar][_longCle];
+		
+		// On remplie le tableau de caractÃ¨re par caractÃ¨re
+		for (int colonne = 0; colonne < _longCle; colonne++) 
+			for (int ligne = 0; ligne < nbLnChar; ligne++) 
+				if (posMsg < _msgTrans.length()) {
+					cMsgs[ligne][colonne] = _msgTrans.charAt(posMsg);
+					posMsg++;
+				}
+				else 
+					cMsgs[ligne][colonne] = '"'; // CaractÃ¨re de msg vide
+		
+		return cMsgs;
+	}
+	
+	// Généré un mapping: clef => code de chiffrement; valeur => position dans la Matrice
+	private static Map GenererMapDecrypte(String _cles, int _longCle) {
+		Map<Integer, Integer> posClefs = new HashMap<Integer,Integer>();
+		
+		for (int i = 0; i < _longCle; i++)
+			posClefs.put(i, Character.getNumericValue(_cles.charAt(i)));
+		
+		return posClefs;
+	}
 }
--- Main.java
+++ Main.java
@@ -2,11 +2,21 @@
 public class Main {
 
 	public static void main(String[] args) {
-		String message = "ce cours de mathÃ©matiques est trÃ¨s intÃ©ressant";
+		String message = "ce cours de mathématiques est très intéressant";
 		String cle = "7 1 4 5 2 3 8 6";
 		
+		// Chiffrement
+		System.out.println("Message encrypte:");
 		String msgEncrype = Chiffrement.chiffrer(message, cle);
 		System.out.println(msgEncrype);
+		
+		System.out.println("\n");
+		
+		// Déchiffrement
+		System.out.println("Message decrypte:");
+		String msgDecrypte = Chiffrement.dechiffrer(msgEncrype, cle);
+		System.out.println(msgDecrypte);
+		
 	}
 
 }
