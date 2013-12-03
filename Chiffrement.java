04b6ae1713816095cf88820432c8691eea022c93
Simoniz <simonblanchette4@gmail.com>
2013-12-03 09:03:14 -0500

    Implémentation du cryptage: Attention, j'ai botter le static

--- Chiffrement.java
+++ Chiffrement.java
@@ -1,13 +1,96 @@
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Chiffrement 
 {
-	public static String chiffrer(String message, String cle)
+	public String chiffrer(String message, String cle)
+	{
+		String msgTransposee;
+		String msgCBC;
+		
+		msgTransposee = ChiffrerClefTransposition(message, cle);
+		msgCBC = ChiffrementCBC(msgTransposee);
+		
+		return msgCBC;
+	}
+	
+	public String dechiffrer(String message, String cle)
 	{
 		return "";
 	}
 	
-	public static String dechiffrer(String message, String cle)
-	{
-		return "";
+	// Transforme la string dans un tableau 2 dimentions de char
+	private char[][] GenererTabChar(String _message, int _longCle) {
+		
+		int longMsgs, nbLnChar, posMsg;
+		longMsgs = _message.length();
+		nbLnChar = (longMsgs / _longCle) + 1;
+		posMsg   = 0;
+		
+		char[][] cMsgs = new char[nbLnChar][_longCle];
+		
+		// On remplie le tableau de caractère par caractère
+		for (int ligne = 0; ligne < nbLnChar; ligne++)
+			for (int colonne = 0; colonne < _longCle; colonne++) 
+				if (posMsg < _message.length()) {
+					cMsgs[ligne][colonne] = _message.charAt(posMsg);
+					posMsg++;
+				}
+				else 
+					cMsgs[ligne][colonne] = '"'; // Caractère de msg vide
+		return cMsgs;
 	}
+	
+	// génère un mapping: clef => code de chiffrement; valeur => position dans la Matrice
+	private Map GenererTabAsso(String _cles, int _longCle) {
+		Map<Integer, Integer> posClefs = new HashMap<Integer,Integer>();
+		
+		for (int i = 0; i < _longCle; i++)
+			posClefs.put(Character.getNumericValue(_cles.charAt(i)), i);
+		
+		return posClefs;
+	}
+
+	// Chiffrement par transposition
+	private String ChiffrerClefTransposition(String _message, String _cle) {
+		String sCles, msgCodee = "";
+		int longCles, colMatrice;
+		char[][] messOrigine;
+		
+		sCles    = _cle.replaceAll("\\s","");
+		longCles = sCles.length();
+		
+		Map<Integer,Integer> posClefs = GenererTabAsso(sCles, longCles);
+		messOrigine = GenererTabChar(_message, longCles);
+		
+		for (int colonne = 0; colonne < messOrigine[0].length; colonne++) {
+			// Se positionne sur la bonne colonne de la matrice 
+			colMatrice = posClefs.get(colonne+1);
+		
+			for (int ligne = 0; ligne < messOrigine.length; ligne++) 
+				if (messOrigine[ligne][colMatrice] != '"')
+					msgCodee += messOrigine[ligne][colMatrice];		
+		}
+		
+		return msgCodee;
+	}
+
+	// Chiffrement par bloc CBC
+	private String ChiffrementCBC(String _msgTranspose) {
+		byte VI = 111;
+		
+		byte[] bMsgTrans = _msgTranspose.getBytes();
+		byte[] bMsgCBC   = new byte[bMsgTrans.length];
+		
+		for (int i = 0; i < bMsgTrans.length; i++){
+			if (i == 0) 
+				bMsgCBC[i] = (byte)(((int)bMsgTrans[i]) ^ ((int)VI));
+			else
+				bMsgCBC[i] = (byte)(((int)bMsgTrans[i]) ^ ((int)bMsgTrans[i-1]));
+		}
+		
+		return new String(bMsgCBC);
+	}
+	
 }
--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String message = "ce cours de mathématiques est très intéressant";
+		String cle = "7 1 4 5 2 3 8 6";
+		
+		Chiffrement chr = new Chiffrement();
+		
+		String msgEncrype = chr.chiffrer(message, cle);
+		System.out.println(msgEncrype);
 	}
 
 }
